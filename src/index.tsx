603c7accd886e6f32184f8efc605db80b3737625
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-08 10:47:39 +0200

    feat: add props type ✨

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
-export function DatePicker() {
+type Props = {
+  format?: string
+}
+
+export function DatePicker({ format = 'YYYY-MM-DD' }: Props) {
   return <div>DATE PICKER OK</div>
 }
