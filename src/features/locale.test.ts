dd4ccf5a54c9c21342ff19d23931bda9f6f4b37b
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-12 07:38:23 +0200

    test: add month checking tests 🚨

--- src/features/locale.test.ts
+++ src/features/locale.test.ts
@@ -6,8 +6,43 @@ import { Locale } from './locale'
 
 describe('Given I want all text in my language', () => {
   describe('When I read the months', () => {
-    it("Should return my language's months names", () => {
-      expect(true).toBeTruthy()
+    it('Should return english months in right order', () => {
+      const L = new Locale({ language: 'en' })
+      ;[
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ].forEach((name, index) => {
+        expect(L.getMonthName(index)).toBe(name)
+      })
+    })
+    it('Should return french months in right order', () => {
+      const L = new Locale({ language: 'fr' })
+      ;[
+        'Janvier',
+        'Février',
+        'Mars',
+        'Avril',
+        'Mai',
+        'Juin',
+        'Juillet',
+        'Août',
+        'Septembre',
+        'Octobre',
+        'Novembre',
+        'Décembre',
+      ].forEach((name, index) => {
+        expect(L.getMonthName(index)).toBe(name)
+      })
     })
   })
 })
