69d8148244bdb3fc3285cf8a6a79554249352e3e
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-12 06:10:17 +0200

    feat: change Locale to class and add method ✨

--- src/features/locale.ts
+++ src/features/locale.ts
@@ -1,31 +1,50 @@
-const WEEK_DAYS = new Map()
-WEEK_DAYS.set('en', [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-])
-WEEK_DAYS.set('fr', [
-  'Dimanche',
-  'Lundi',
-  'Mardi',
-  'Mercredi',
-  'Jeudi',
-  'Vendredi',
-  'Samedi',
-])
+export class L {
+  language: string
+  languagesAvailables: Array<string>;
+  [key: string]:
+    | string
+    | Array<string>
+    | Map<string, Array<string>>
+    | (() => Array<string> | undefined)
+    | ((weekStartingDay: string) => number)
 
-export function getWeekStartingDayNumber(weekStartingDay: string) {
-  let index = -1
-  ;[...WEEK_DAYS].filter((locale) => {
-    locale.filter((week) => {
-      if (week.indexOf(weekStartingDay) != -1) {
-        index = week.indexOf(weekStartingDay)
-      }
-    })
-  })
-  return index != -1 ? index : 1
+  constructor(language: string) {
+    this.languagesAvailables = ['en', 'fr']
+    if (this.languagesAvailables.indexOf(language) === -1) {
+      console.error('Unavailable Language, default to english.')
+      this.language = 'en'
+    } else {
+      this.language = language
+    }
+    this.en = new Map<'weekDays', string[]>()
+    this.en.set('weekDays', [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ])
+    this.fr = new Map<'weekDays', string[]>()
+    this.fr.set('weekDays', [
+      'Dimanche',
+      'Lundi',
+      'Mardi',
+      'Mercredi',
+      'Jeudi',
+      'Vendredi',
+      'Samedi',
+    ])
+  }
+
+  getWeekDays(): Array<string> {
+    return (this[this.language] as Map<string, Array<string>>).get(
+      'weekDays'
+    ) as string[]
+  }
+
+  getWeekStartingDayNumber(weekStartingDay: string): number {
+    return this.getWeekDays().indexOf(weekStartingDay)
+  }
 }
