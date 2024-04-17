3fcaf4f6a016a56e577a870ff3acc5cc6728ac15
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-17 06:10:22 +0200

    fix: store get aren't functions 🐛

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -17,7 +17,7 @@ export const DatePickerView = observer(
             placeholder={L.getInputPlaceholder()}
             readOnly={true}
             ref={ref}
-            value={store.dateHaveBeenPicked ? store.formatedDate() : ''}
+            value={store.dateHaveBeenPicked ? store.formatedDate : ''}
           />
           <div
             className={
