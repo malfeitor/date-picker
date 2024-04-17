ece5235d1f167bb12951fa37db05c7c14ceb8498
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-04-17 05:36:01 +0200

    feat: create new view ✨

---
+++ src/view/index.tsx
@@ -0,0 +1,40 @@
+import React, { LegacyRef } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { DatePickerViewProps } from '../types'
+import Header from '../components/Header'
+
+export const DatePickerView = observer(
+  React.forwardRef(
+    ({ store, L }: DatePickerViewProps, ref: LegacyRef<HTMLInputElement>) => {
+      return (
+        <div>
+          <input
+            type="text"
+            className="date-picker__input"
+            onFocus={() => store.setPickerVisibility(true)}
+            placeholder={L.getInputPlaceholder()}
+            readOnly={true}
+            ref={ref}
+          />
+          <div
+            className={
+              store.pickerVisible ? 'date-picker' : 'date-picker__hidden'
+            }
+          >
+            <Header store={store} minimumYear={1950} L={L} />
+            <table className="date-picker__calendar">
+              <thead className="date-picker__calendar--day-names">
+                <tr>
+                  {L.getShortWeekDays().map((day: string, index: number) => (
+                    <th key={`day-name-${index}`}>{day}</th>
+                  ))}
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      )
+    }
+  )
+)
