c1c5cc4af43608f3a58e54de7024b8f422031f9c
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-07 05:17:10 +0200

    feat: disable selects when picker is hidden ✨

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -35,6 +35,7 @@ export const Header = observer(({ store, minimumYear, L }: Props) => {
         defaultValue={store.getDate.getMonth()}
         onChange={(e) => store.setMonth(Number(e.target.value))}
         ref={monthRef}
+        disabled={!store.pickerVisible}
       >
         {L.getAllMonthsNames().map((name: string, index: number) => {
           return (
@@ -49,6 +50,7 @@ export const Header = observer(({ store, minimumYear, L }: Props) => {
         defaultValue={store.getDate.getFullYear()}
         onChange={(e) => store.setYear(Number(e.target.value))}
         ref={yearRef}
+        disabled={!store.pickerVisible}
       >
         {[...Array(100)].map((_, index) => {
           return (
