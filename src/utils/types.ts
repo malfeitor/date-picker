1018fc5329011284e49e352836a7d4d243562a06
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-07 06:40:54 +0200

    refactor: merge types definitions 📦

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,6 +2,13 @@ export interface InputRef {
   current: HTMLInputElement
 }
 
+import { Locale } from '../features/locale'
+import { Store } from '../store'
+
+export interface InputRef {
+  current: HTMLInputElement
+}
+
 export type Day = {
   day: string
   number: number
@@ -13,4 +20,18 @@ export function isInputRef(ref: any): ref is InputRef {
   } else {
     return false
   }
+}
+
+export interface DatePickerViewProps {
+  store: Store
+  L: Locale
+  minimumYear: number
+}
+
+export interface DatePickerProps
+  extends React.ComponentPropsWithoutRef<'input'> {
+  format?: string
+  weekStartingDay?: string
+  language?: string
+  minimumYear?: number
 }
