1eb5851ee22478383bc42c8660dfbaef6e02813b
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-27 06:10:33 +0200

    feat: move datepicker content if overflow window ✨

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,4 +1,4 @@
-import React, { Ref } from 'react'
+import React, { Ref, useEffect, useRef, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { DatePickerViewProps } from '../utils/types'
@@ -6,6 +6,7 @@ import { Header } from '../components/Header'
 import { Calendar } from '../components/Calendar'
 import mimicInputReadOnly from '../utils/mimicInputReadOnly'
 import { Form } from 'react-bootstrap'
+import { reaction } from 'mobx'
 
 export const DatePickerView = observer(
   React.forwardRef(
@@ -19,6 +20,45 @@ export const DatePickerView = observer(
       }: DatePickerViewProps,
       ref: Ref<HTMLInputElement>
     ) => {
+      const datePickerContentRef = useRef(null)
+      const [contentStyle, setContentStyle] = useState({
+        display: 'none',
+      })
+
+      reaction(
+        () => store.pickerVisible,
+        (visible) => {
+          if (visible) {
+            setContentStyle((contentStyle) => ({
+              ...contentStyle,
+              display: 'block',
+            }))
+          } else {
+            setContentStyle((contentStyle) => ({
+              ...contentStyle,
+              display: 'none',
+            }))
+          }
+        }
+      )
+
+      useEffect(() => {
+        const documentBodyWidth = document.body.clientWidth
+        const { x, width } = (
+          datePickerContentRef.current as HTMLInputElement
+        ).getBoundingClientRect()
+        if (x + width > documentBodyWidth) {
+          const distanceFromLeft = documentBodyWidth - (x + width)
+          const newStyle = {
+            ...contentStyle,
+            left: distanceFromLeft,
+          }
+          if (JSON.stringify(newStyle) !== JSON.stringify(contentStyle)) {
+            setContentStyle(newStyle)
+          }
+        }
+      }, [contentStyle])
+
       return (
         <div className="date-picker">
           <Form.Control
@@ -36,10 +76,9 @@ export const DatePickerView = observer(
             {errorInvalidDate}
           </Form.Control.Feedback>
           <div
-            style={{
-              display: store.pickerVisible ? 'block' : 'none',
-            }}
+            style={contentStyle}
             className="date-picker__content"
+            ref={datePickerContentRef}
           >
             <Header store={store} minimumYear={minimumYear} L={L} />
             <table className="date-picker__calendar">
