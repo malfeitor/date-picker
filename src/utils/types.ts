816c592eff3979f4f519f2a0a410972dc089d39e
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-27 03:54:52 +0200

    fix: correct TS Ref errors 🐛

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,6 +2,7 @@ export interface InputRef {
   current: HTMLInputElement
 }
 
+import { Ref } from 'react'
 import { Locale } from '../features/locale'
 import { Store } from '../store'
 
@@ -22,11 +23,12 @@ export function isInputRef(ref: any): ref is InputRef {
   }
 }
 
-export interface DatePickerViewProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface DatePickerViewProps {
+  ref: Ref<HTMLInputElement>
   store: Store
   L: Locale
   minimumYear: number
+  errorInvalidDate: string
 }
 
 export interface DatePickerProps
--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,29 +1,39 @@
-import React, { LegacyRef } from 'react'
+import React, { Ref } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { DatePickerViewProps } from '../utils/types'
 import { Header } from '../components/Header'
 import { Calendar } from '../components/Calendar'
 import mimicInputReadOnly from '../utils/mimicInputReadOnly'
+import { Form } from 'react-bootstrap'
 
 export const DatePickerView = observer(
   React.forwardRef(
     (
-      { store, L, minimumYear, ...restProps }: DatePickerViewProps,
-      ref: LegacyRef<HTMLInputElement>
+      {
+        store,
+        L,
+        minimumYear,
+        errorInvalidDate = 'Error, invalid date',
+        ...restProps
+      }: DatePickerViewProps,
+      ref: Ref<HTMLInputElement>
     ) => {
       return (
         <div className="date-picker">
-          <input
+          <Form.Control
             name="datepicker-input"
             type="text"
             className="date-picker__input"
             onFocus={() => store.setPickerVisibility(true)}
             placeholder={L.getInputPlaceholder()}
             onKeyDown={(e) => mimicInputReadOnly(e)}
-            ref={ref}
             {...restProps}
+            ref={ref}
           />
+          <Form.Control.Feedback type="invalid">
+            {errorInvalidDate}
+          </Form.Control.Feedback>
           <div
             style={{
               display: store.pickerVisible ? 'block' : 'none',
